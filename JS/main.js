56625d53f155cdaa31fc19c50293011ca02a97a8
Eloyse-981 <eloysevitoria0211@gmail.com>
2024-05-02 15:32:17 -0300

    criado o main.js

---
+++ JS/main.js
@@ -0,0 +1,53 @@
+var nome = prompt('insira seu nome')
+var senha = prompt('Insira sua senha')
+
+var meuLogin = 'Eloyse'
+var minhaSenha = '1107'
+
+if(nome == meuLogin && senha == minhaSenha){
+    console.log('Estou logado')
+}else{
+    console.log('Senha incorreta!')
+}
+
+var nome = prompt('insira seu nome')
+var senha = prompt('Insira sua senha')
+
+var meuLogin = 'Eloyse'
+var minhaSenha = '1107'
+
+if(nome == meuLogin && senha == minhaSenha){
+    console.log('Estou logado')
+}else{
+    console.log('Senha incorreta!')
+}
+
+var nome = prompt('insira seu nome')
+var senha = prompt('Insira sua senha')
+
+var meuLogin = 'Eloyse'
+var minhaSenha = '1107'
+
+if(nome == meuLogin && senha == minhaSenha){
+    console.log('Estou logado')
+}else{
+    alert('Excesso de entrada')
+}
+    
+
+
+// nome = 'Eloyse'
+
+// function calcularMedia(n1, n2, n3){
+//     return (n1 + n2 + n3) / 3
+// }
+
+// var media = calcularMedia(4, 5, 10)
+
+// if(media >= 8){
+//     resultado = 'Aprovado'
+// }else{
+//     resultado = 'Reprovado'
+// }
+
+// console.log('O resultado da sua média é ', media, ' Sua situação é: ', resultado)
